26266e19c3ec48cc438870d332b3895aad1dac52
night-slayer18 <samanuaia257@gmail.com>
2025-03-24 20:55:08 +0530

    For query execution

---
+++ src/utils/executeQuery.ts
@@ -0,0 +1,27 @@
+import { getConnection } from "../connections/connectionManager";
+import { executeMongoQuery } from "../helpers/mongodbHelper";
+import { executeCassandraQuery } from "../helpers/cassandraHelper";
+
+/**
+ * Executes a query using the connection determined by the tenant context.
+ * For PostgreSQL and MySQL, the native connection.query is used.
+ * For MongoDB and Cassandra, corresponding helper functions are invoked.
+ */
+export async function executeQuery(query: string | any, params?: any[]): Promise<any> {
+  // getConnection now returns an object with { connection, dbType }
+  const { connection, dbType } = await getConnection();
+  
+  switch (dbType) {
+    case "postgres":
+    case "mysql":
+      // For these, query is a string and params is an array
+      return connection.query(query, params);
+    case "mongodb":
+      // For MongoDB, expect query to be an object with collection, method, args.
+      return executeMongoQuery(connection, query);
+    case "cassandra":
+      return executeCassandraQuery(connection, query, params);
+    default:
+      throw new Error(`Unsupported database type: ${dbType}`);
+  }
+}
